46a675a822c9acbb4e04ad56fbb0628117c99f0a
Thailane Lopes Dutra <thailane111122@gmail.com>
2020-08-11 21:45:20 -0300

    Update DiaSemana.java

--- DiaSemana.java
+++ DiaSemana.java
@@ -41,3 +41,45 @@ public class DiaSemana {
 
 }
 
+VERSÃO DO PROGRAMA UTILIZANDO A ESTRUTURA SWITCH-CASE.
+	
+	import java.util.Scanner;
+
+public class DiaSemana {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		String dia;
+		
+		switch (x) { //switch, O switch vai funcionar como um interruptor, pois dependendo da entrada que você der a ele, ele vai acionar somente certo(s) comando(s) dentre os que você disponibilizou.
+		case 1:
+			dia = "domingo";
+			break;  //break, é obrigatorio quando se está utilizando a estrutura switch-case.
+		case 2:
+			dia = "segunda";
+			break;
+		case 3: //case, condição caso aconteça x então processar y.
+			dia = "terca";
+			break;
+		case 4:
+			dia = "quarta";
+			break;
+		case 5:
+			dia = "quinta";
+			break;
+		case 6:
+			dia = "sexta";
+			break;
+		case 7:
+			dia = "sabado";
+			break;
+		default: //Se os valores determinados acima estiverem incorretos, o default processará a condição abaixo.
+			dia = "valor invalido";
+			break;
+		}
+		System.out.println("Dia da semana: " + dia);
+		sc.close();
+	}
+}
+
