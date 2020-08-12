193e6956dea7a723f6d016c27f3d182001f8d3be
Thailane Lopes Dutra <thailane111122@gmail.com>
2020-08-11 22:05:40 -0300

    Update CondicTermaria.java
    version

--- CondicTermaria.java
+++ CondicTermaria.java
@@ -22,3 +22,27 @@ public class CondicTermaria {
 	}
 
 }
+
+
+Versão do programa utilizando a condição Termária:
+
+
+import java.util.Scanner;
+
+public class CondicTermaria {
+
+	public static void main(String[] args) {
+
+		Scanner teclado = new Scanner(System.in);
+		
+		
+		double preco = 34.5;
+		double desconto = (preco < 20.00) ? preco * 0.1 : preco * 0.05; // aqui foi aplicado a condição termária, onde conforme a condição, compara se é verdadeiro ou falso: desconto = (preco < 20.00) ? preco * 0.1 : preco * 0.05;
+		
+		
+		System.out.println(desconto);
+
+		teclado.close();
+	}
+
+}
